da17c81bdc7b1201560644fa2e8920d89c52fcdd
jiffies <lcqtdwj@gmail.com>
2012-08-24 17:32:18 +0800

    first

---
+++ myscript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var now = new Date;
+	var deadline = new Date(2013,0,26);
+	var days = diffDays(now,deadline);
+	
+	$('<p id="date"></p>').appendTo('body');
+	//$('<p id="time"></p>').appendTo('body');
+	$('#date').text('距离2013年考研还有'+days+'天');
+	//$('#time').text(now.toLocaleTimeString());
+});
+
+function diffDays(predate, olddate){
+	var diff = olddate - predate;
+	var days = diff/1000/60/60/24;
+	return days.toFixed();
+}
